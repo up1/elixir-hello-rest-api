4c743b92cb0caec87f6d2eeff7d114e8c62b8716
up1 <somkiat.p@gmail.com>
2019-06-09 16:20:35 +0700

    Remove unused

--- lib/hello/endpoint.ex
+++ lib/hello/endpoint.ex
@@ -1,40 +1,16 @@
 defmodule Hello.Endpoint do
   
   use Plug.Router
-  plug(Plug.Logger)
+
   plug(:match)
-  plug(Plug.Parsers, parsers: [:json], json_decoder: Poison)
   plug(:dispatch)
+  plug(Plug.Parsers, parsers: [:json], json_decoder: Poison)
 
   get "/ping" do
     send_resp(conn, 200, Poison.encode!(%{response: "Pong!"}))
   end
 
-  post "/events" do
-    {status, body} =
-      case conn.body_params do
-        %{"events" => events} -> {200, process_events(events)}
-        _ -> {422, missing_events()}
-      end
-
-    send_resp(conn, status, body)
-  end
-
-  defp process_events(events) when is_list(events) do
-    Poison.encode!(%{response: "Received Events!"})
-  end
-
-  defp process_events(_) do
-    Poison.encode!(%{response: "Please Send Some Events!"})
-  end
-
-  defp missing_events do
-    Poison.encode!(%{error: "Expected Payload: { 'events': [...] }"})
-  end
-
-  # A catchall route, 'match' will match no matter the request method,
-  # so a response is always returned, even if there is no route to match.
   match _ do
-    send_resp(conn, 404, "oops... Nothing here :(")
+    send_resp(conn, 404, "Page not found !!")
   end
 end
